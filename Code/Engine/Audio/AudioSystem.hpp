bf529c34ae9fab5e3b4076ed48ca9cf887edbfd9
achasedev <amchase@smu.edu>
2019-02-06 18:19:05 -0600

    Removed FFT implementation from AudioSystem.

--- Code/Engine/Audio/AudioSystem.hpp
+++ Code/Engine/Audio/AudioSystem.hpp
@@ -5,9 +5,6 @@
 #include <map>
 #include <string>
 #include <vector>
-#include "Engine/Math/AABB2.hpp"
-#include "Engine/Math/IntVector2.hpp"
-#include "Engine/Rendering/Meshes/Mesh.hpp"
 #include "Engine/Core/Utility/XmlUtilities.hpp"
 #include "ThirdParty/fmod/fmod.hpp"
 
@@ -32,8 +29,6 @@ public:
 	static void					LoadAudioGroupFile(const std::string& filepath);
 
 	virtual void				BeginFrame();
-	virtual void				ProcessInput();
-	virtual void				RenderFFTGraph() const;
 	virtual void				EndFrame();
 
 	virtual SoundID				CreateOrGetSound( const std::string& soundFilePath );
@@ -47,14 +42,6 @@ public:
 	virtual void				ValidateResult( FMOD_RESULT result );
 
 	FMOD::System*				GetFMODSystem() const;
-
-	// FFT
-	void						SetFFTMaxXValue(float maxFrequency);
-	void						SetFFTMaxYValue(float newValue);
-	void						SetWindowSize(int windowSize);
-	void						SetFFTWindowType(FMOD_DSP_FFT_WINDOW windowType);
-	static void					SetShouldRender(bool newState);
-	static bool					ShouldRender();
 	
 
 protected:
@@ -64,55 +51,14 @@ protected:
 
 	std::map<std::string, AudioGroup*>	m_audioGroups;
 
-	// For FFT rendering
-	FMOD::DSP*							m_fftDSP = nullptr;
-	FMOD_DSP_PARAMETER_FFT*				m_spectrumData = nullptr;
-	bool								m_renderFFTGraph = false;
-	mutable Mesh						m_barMesh;
-	mutable Mesh						m_gridMesh;
-	unsigned int						m_numWindowSegments = 4096;
-	unsigned int						m_segmentsToDisplay = 512;
-	float								m_maxValueLastFrame = 0.f;
-	float								m_sampleRate = 48000.f;
-	float								m_nyquistFreq = m_sampleRate * 0.5f;
 
-	// UI Settings
-	float								m_graphHeight = 800.f;
-	float								m_fontHeight = 30.f;
-	Vector2								m_screenEdgePadding = Vector2(30.f, 100.f);
-	float								m_fftMaxYAxis = 1.0f;
-
-	AABB2 m_totalBounds;
-	AABB2 m_maxValueBounds;
-	AABB2 m_graphBounds;
-	AABB2 m_headingBounds;
-
-	AABB2 m_xAxisBounds;
-	AABB2 m_yAxisBounds;
-	AABB2 m_rightSidePanel;
-
-	Rgba m_lineAndPanelColor = Rgba(15, 60, 120, 200);
-	Rgba m_backgroundColor = Rgba(0, 0, 0, 100);
-	Rgba m_fontColor = Rgba(200, 200, 200, 200);
-
-	IntVector2 m_gridSegmentCount = IntVector2(20, 10);
-
-
-private:
-	//-----Private Methods-----
+protected:
+	//-----Protected Methods-----
 
 	// Use Initialize/Destroy instead
 	AudioSystem();
 	virtual ~AudioSystem();
 	AudioSystem(const AudioSystem& copy) = delete;
-
-	// Console Commands
-	static void InitializeConsoleCommands();
-
-	// For FFT rendering
-	void SetupUIBounds();
-	void AddFFTDSPToMasterChannel();
-	void UpdateFFTGraph();
 
 
 private:
